bdcc971ea749dd72aea956dbf95cbb8692f2157d
Aarjav Jain <anshjain638@gmail.com>
2024-10-24 00:25:47 +0530

    fix: added undefined validation in payment method

--- components/Checkout/Payment/PaymentMethod.tsx
+++ components/Checkout/Payment/PaymentMethod.tsx
@@ -17,35 +17,36 @@ const PaymentMethod = ({
         alignItems: 'center',
         width: '100%',
       }}>
-      {paymentMethods.map((method, index) => {
-        return (
-          <TouchableOpacity
-            key={index}
-            style={{
-              justifyContent: 'center',
-              alignItems: 'center',
-              backgroundColor:
-                selectedMethod === method.id ? '#FA4248' : '#fff',
-              borderColor: '#DCDCDC',
-              padding: 10,
-              borderWidth: 1,
-              height: 60,
-              width: 90,
-              borderRadius: 25,
-            }}
-            onPress={() => setSelectedMethod(method.id)}>
-            <Image
-              source={method.icon}
+      {paymentMethods &&
+        paymentMethods.map((method, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
               style={{
-                width: 20,
-                height: 20,
-                tintColor: selectedMethod === method.id ? '#fff' : '#ddd',
-                resizeMode: 'contain',
+                justifyContent: 'center',
+                alignItems: 'center',
+                backgroundColor:
+                  selectedMethod === method.id ? '#FA4248' : '#fff',
+                borderColor: '#DCDCDC',
+                padding: 10,
+                borderWidth: 1,
+                height: 60,
+                width: 90,
+                borderRadius: 25,
               }}
-            />
-          </TouchableOpacity>
-        );
-      })}
+              onPress={() => setSelectedMethod(method.id)}>
+              <Image
+                source={method.icon}
+                style={{
+                  width: 20,
+                  height: 20,
+                  tintColor: selectedMethod === method.id ? '#fff' : '#ddd',
+                  resizeMode: 'contain',
+                }}
+              />
+            </TouchableOpacity>
+          );
+        })}
     </View>
   );
 };
